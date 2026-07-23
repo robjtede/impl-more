e2a1e6ab7546e8ae6c8687ea64a5a4ff145a7812
Rob Ede <robjtede@icloud.com>
2026-07-24 00:29:03 +0100

    docs: separate FromStr example

--- crates/impl-more/src/lib.rs
+++ crates/impl-more/src/lib.rs
@@ -22,6 +22,7 @@
 //!
 //! impl_more::impl_from!(String => MyNewTypeStruct);
 //! impl_more::impl_into!(MyNewTypeStruct => String);
+//!
 //! impl_more::forward_from_str!(MyNewTypeStruct => String);
 //!
 //! enum MyEnum {
